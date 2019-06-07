d42af9e0610db21291063fd61970daa46566a3a7
guizg <guizgraicer@gmail.com>
2019-06-07 09:33:27 -0300

    as

--- questao1.cu
+++ questao1.cu
@@ -21,7 +21,7 @@ __device__ void d_print(bool** grid){
     printf("\n\n\n\n\n");
     for(unsigned int i = 1; i < size-1; i++) {
       for(unsigned int j = 1; j < size-1; j++)
-        printf(grid[i][j]?"#":"_");
+        grid[i][j]?printf("#"):printf("_");
       printf("\n");
     }
   }
